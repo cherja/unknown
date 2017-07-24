8308a59e41b368b46b60d7bc1280a9a43b690256
Виталий Чернявский <4irik91@mail.ru>
2017-07-24 10:42:01 +0400

    Убрал гамбургер,Убрал с  font ccылки и файлы

--- js/index.js
+++
@@ -1,9 +0,0 @@
-var hamburger = document.querySelector(".hamburger");
-hamburger.addEventListener("click", function() {
-  hamburger.classList.toggle("is-active");
-});
-
-var header = document.querySelector("header");
-hamburger.addEventListener("click", function() {
-  header.classList.toggle("active");
-});
