e239af33a899dc2a7ff1ca455d5b290150dceebb
Виталий Чернявский <4irik91@mail.ru>
2017-08-10 20:29:26 +0400

    Всплывающая форма

---
+++ js/index.js
@@ -0,0 +1,5 @@
+var modalform = document.querySelector(".modalform"),
+    buttonmodal = document.querySelector(".buttonmodal");
+buttonmodal.addEventListener("click", function() {
+  modalform.classList.toggle("active");
+});
