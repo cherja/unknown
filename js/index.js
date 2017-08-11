61522a68b7a37957dd00be672c01be9605aa63d1
Виталий Чернявский <4irik91@mail.ru>
2017-08-11 16:59:28 +0400

    Модальная форма на кнопке и на ссылке

--- js/index.js
+++ js/index.js
@@ -1,5 +1,20 @@
-var modalform = document.querySelector(".modalform"),
-    buttonmodal = document.querySelector(".buttonmodal");
-buttonmodal.addEventListener("click", function() {
-  modalform.classList.toggle("active");
-});
+
+//  Инициализация модального окна
+var
+  modal            = new tingle.modal({ closeMethods: ['overlay', 'button', 'escape'] }),
+  modalContentWrap = document.getElementById('modal'),         //  Обертка формы
+  triggers         = document.querySelectorAll('.openRegForm') //  Кнопки открытия окна
+
+// Перемещаем форму из DOM в модалку
+modal.setContent(modalContentWrap.innerHTML)
+
+// Удаляем копию формы
+modalContentWrap.parentNode.removeChild(modalContentWrap)
+
+//  Кнопки открывают окно по клику
+triggers.forEach(function(trigger) {
+  trigger.addEventListener('click', function(e) {
+    e.preventDefault();
+    modal.open()
+  })
+})
