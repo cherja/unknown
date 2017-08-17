7ed878770ee3e028385852caeeb17a300f1abcf5
klimash <andrey.klimash@gmail.com>
2017-08-17 12:46:56 +0300

    Откорректировал Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,15 +3,15 @@ var
   browserSync  = require('browser-sync'),               //  Для hot-reload
   stylus       = require('gulp-stylus'),                //  Компилирует STYLUS
   concat       = require('gulp-concat'),                //  Объединяет файлы
-  uglify       = require('gulp-uglifyjs'),              //  Минифицирует js
+  uglify       = require('gulp-uglify'),                //  Минифицирует js
   csso         = require('gulp-csso'),                  //  Минифицирует css
   del          = require('del'),                        //  Удаляет файлы
   imagemin     = require('gulp-imagemin'),              //  Минифицирует изображения
+  imageminJR   = require('imagemin-jpeg-recompress'),   //  Доп. минификация JPEG
   Pngquant     = require('imagemin-pngquant'),          //  Доп. минификация PNG
   autoprefixer = require('gulp-autoprefixer'),          //  Подставляет префиксы для css
   uncss        = require('gulp-uncss'),                 //  Отсеивает неиспользуемые стили
   babel        = require('gulp-babel'),                 //  Транспилирует ES6 в ES3
-  htmlmin      = require('gulp-html-minifier'),         //  Минифицирует HTML
   rigger       = require('gulp-rigger'),                //  Импортирует файлы
   replace      = require('gulp-replace');               //  Для версионирования
 
@@ -72,7 +72,7 @@ gulp.task('css', ['main.css', 'vendor.css'], function() {
 gulp.task('js', ['main.js', 'vendor.js'], function() {
   return gulp
     .src('app/js/*.js')
-    // .pipe(uglify())
+    .pipe(uglify())
     .pipe(gulp.dest('dist/js/'));
 });
 
@@ -93,23 +93,30 @@ gulp.task('img', function() {
     .src('app/img/*')
     .pipe(gulp.dest('dist/img/'))
     .pipe(imagemin([
+      imageminJR({ progressive: true, max: 80, min: 70 }),
       Pngquant({quality: '80'})
     ]))
     .pipe(gulp.dest('dist/img/'));
 });
 
-gulp.task('html', function() {
+gulp.task('html-dev', function () {
+  return gulp
+    .src('app/html/[^_]*.html')
+    .pipe(rigger())
+    .pipe(gulp.dest('app/'))
+    .pipe(browserSync.reload({ stream: true }));    
+});
+
+gulp.task('html', ['html-dev'], function() {
   return gulp
     .src('app/*.html')
-    .pipe(rigger())
     .pipe(replace(/{ver}/g, Date.now()))
-    .pipe(htmlmin({ collapseWhitespace: true }))
     .pipe(gulp.dest('dist/'));
 });
 
 gulp.task('static', function() {
   return gulp
-    .src(['app/static/**/*', 'app/*.*'])
+    .src(['app/*.*', '!app/*.html'])
     .pipe(gulp.dest('dist/'));
 });
 
@@ -119,10 +126,10 @@ gulp.task('favicons', function() {
     .pipe(gulp.dest('dist/favicons/'));
 });
 
-gulp.task('default', ['browser-sync', 'main.css', 'vendor.css', 'main.js', 'vendor.js', 'html'], function() {
+gulp.task('default', ['browser-sync', 'main.css', 'vendor.css', 'main.js', 'vendor.js', 'html-dev'], function() {
   gulp.watch('app/styl/**/*.styl', ['main.css']);
   gulp.watch('app/js/modules/*.js', ['main.js']);
-  gulp.watch('app/*.html', browserSync.reload);
+  gulp.watch('app/html/**/*.html', ['html-dev']);
 });
 
-gulp.task('build', ['clear', 'css', 'js', 'fonts', 'php', 'img', 'html', 'static', 'favicons']);
+gulp.task('build', ['clear', 'css', 'js', 'fonts', 'php', 'img', 'html-dev', 'html', 'static', 'favicons']);
