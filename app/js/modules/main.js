2cbcc8529d2b91ca17db5c36bbce7719b43b736d
Unknown <andrey.klimash@gmail.com>
2017-09-13 14:48:41 +0300

    + указание суммы для нал. платежа

--- app/js/modules/main.js
+++ app/js/modules/main.js
@@ -1,7 +1,17 @@
 (() => {
+
 //= calculator.js
 //= initform.js
 //= bottom.js
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('isCashPay').onchange = event => {
+        const elemToShow = document.getElementById('cashPaySumm');
+        elemToShow.style.display = event.target.checked ? 'block' : 'none';
+        elemToShow.children[0].focus();
+    }
+});
+
 })()
 
 
